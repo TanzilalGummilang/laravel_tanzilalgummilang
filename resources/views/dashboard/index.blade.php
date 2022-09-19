9c703f6b65fc9cbd86cd8d3eb11f8cf28f55b907
TanzilalGummilang <tanzilalgummilang@gmail.com>
2022-09-19 14:37:39 +0700

    layouts, dashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+@section('title')
+  Dashboard
+@endsection
+
+@section('main-content')
+  dashboard
+@endsection
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,57 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="/dashboard">Soal B</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+
+        <li class="nav-item">
+          <a class="nav-link " href="#">Rumah Sakit</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link " href="#">Pasien</a>
+        </li>
+
+        {{-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Blog
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li>
+              <a class="dropdown-item " href="#">Post</a>
+            </li>
+            <li><hr class="dropdown-divider"></li>
+            <li>
+              <a class="dropdown-item " href="#">Category</a>
+            </li>
+            <li><a class="dropdown-item " href="#">Author</a></li>
+          </ul>
+        </li> --}}
+
+      </ul>
+
+      <ul class="navbar-nav ms-auto">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Hallo, {{ auth()->user()->name }}
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <form action="{{ route('logout') }}" method="POST">
+                  @csrf
+                  <button type="submit" class="dropdown-item">
+                    <i class="bi bi-box-arrow-right"></i> Logout
+                  </button>
+                </form>
+              </li>
+            </ul>
+          </li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
