5c1adbed972b89cbe9af157331bfbc6c0c10303c
TanzilalGummilang <tanzilalgummilang@gmail.com>
2022-09-19 21:52:03 +0700

    update dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,5 +4,27 @@
 @endsection
 
 @section('main-content')
-  dashboard
+  <div class="main-content">
+
+    <div class="content-wrapper">
+      <div class="row same-height">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header mt-2 mb-2">
+              <h4>@yield('title')</h4>
+            </div>
+            <div class="card-body mb-1">
+
+              <h5 class="mb-3 mt-1">PT Terakorp Indonesia</h5>
+              <h6 class="fw-normal">Soal Coding Test screening.teramedik.com</h6>
+              <h6 class="fw-normal">Screening Web Programmer</h6>
+              <h6 class="fw-normal">Soal B</h6>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
 @endsection
