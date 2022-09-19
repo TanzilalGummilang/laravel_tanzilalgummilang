74c9dc1984ef40ea3dddca6da75227179c95a7b6
TanzilalGummilang <tanzilalgummilang@gmail.com>
2022-09-19 20:33:10 +0700

    update login via username

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,10 +16,17 @@
             @csrf
 
             <!-- Email Address -->
-            <div>
+            {{-- <div>
                 <x-input-label for="email" :value="__('Email')" />
 
                 <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div> --}}
+
+            <!-- username  -->
+            <div>
+                <x-input-label for="name" :value="__('Username')" />
+
+                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
             </div>
 
             <!-- Password -->
@@ -33,11 +40,14 @@
             </div>
 
             <!-- Remember Me -->
-            <div class="block mt-4">
+            <div class="flex mt-4 justify-between">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    {{ __('Not Registered?') }}
+                </a>
             </div>
 
             <div class="flex items-center justify-end mt-4">
